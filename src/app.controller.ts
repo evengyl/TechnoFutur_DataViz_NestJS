92345b714a55d1dacf663f2fcb0a80cd33f4d392
evengyl <dark.evengyl@gmail.com>
2022-02-18 11:10:50 +0100

    imp controller app demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller("api")
+@Controller("api/todos")
 export class AppController 
 {
-  constructor(private appService: AppService) {}
+  constructor() {}
+  /*
+    GET:localhost:3000/api/todos/         ->get all todos GET
+    GET:localhost:3000/api/todos/42       ->get one todo GET
+    POST:localhost:3000/api/todos/        ->post un nouveau todo POST
+    PUT/PATCH:localhost:3000/api/todos/42 -> put/patch permet la modification d'un todo PUT/PATCH
+    DELETE:localhost:3000/api/todos/42    -> delete un todo DELETE
+  */
 
-  @Get("methode1")
-  getHello() : string
-  {
-    return this.appService.tutupompidou();
-  }
+    @Get()
+    getAll() : any[]
+    {
+      return ["todo 1", "todo 2", "todo 3"]
+    }
 
-  @Get("methode2")
-  getTutu() : any
-  {
-    return {"message" : "j'en ai marre de chercher après des idées de variables"}
-  }
+    @Get(":id")
+    getOneById(@Param('id') id) : any
+    {
+      return `todo ${id}`
+    }
+
+    @Post()
+    postTodo(@Body() newTodo) : any
+    {
+      console.log(newTodo)
+      return "Nouveau todo créé"
+    }
+
+    @Patch(':id')
+    updateTodo(@Param('id') id, @Body() updateTodo) : any
+    {
+      console.log(`Todo à modifier : ${id}`)
+      console.log(`Corp de la todo : `)
+      console.log(updateTodo)
+      return `Todo ${id} bien modifiée`
+    }
+
+
+    @Delete(':id')
+    deleteTodo(@Param("id") id) : any
+    {
+      return `Todo ${id} bien supprimée`
+    }
+
+  
 }
